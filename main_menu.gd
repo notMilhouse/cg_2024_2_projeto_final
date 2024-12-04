d5173afbae9a784d9a67ebf6dc1d6ddaea3e2ae8
Gabriel Carvalho <gabriel_carvalho@usp.br>
2024-12-04 16:27:20 -0300

    final project setup

---
+++ main_menu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_play_caverna_pressed() -> void:
+	get_tree().change_scene_to_file("res://niveis/sample/sample.tscn")
---
+++ niveis/sample/kaka.gd
@@ -0,0 +1,56 @@
+extends CharacterBody3D
+
+signal hit
+signal catch
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.basis = Basis.looking_at(direction)
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
+	
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If the collision is with ground
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			mob.die()
+			hit.emit()
+			break
+		if collision.get_collider().is_in_group("amigo"):
+			var amigo = collision.get_collider()
+			amigo.die()
+			catch.emit()
+			break
